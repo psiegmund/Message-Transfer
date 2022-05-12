7f6dbe771934b19449033ef94be0e5aa9788f78b
psiegmund <patrick-siegmund@web.de>
2022-05-12 13:03:11 +0200

    function als SWITCH() hinzugefügt

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,38 @@ const messageInput = document.getElementById('message-input');
 
 
 
+
+// function showtxt() {
+
+//     console.log(Boolean(messageInput.value));
+//     if (messageInput.value) {
+//         messageOutput.innerHTML = messageInput.value
+//         messageAlarm.style.visibility = 'hidden'
+//         messageOutput.style.visibility = 'visible'
+//     }
+
+//     else {
+//         messageAlarm.innerHTML = 'Bitte gib eine Nachricht ein!';
+//         messageOutput.style.visibility = 'hidden'
+//         messageAlarm.style.visibility = 'visible'
+//     }
+// }
+
 function showtxt() {
+    switch (Boolean(messageInput.value)) {
+        case true:
+            console.log('is TRUE');
+            messageOutput.innerHTML = messageInput.value
+            messageAlarm.style.visibility = 'hidden'
+            messageOutput.style.visibility = 'visible'
+            break;
 
-    console.log(messageInput.value);
-    if (messageInput.value) {
-        messageOutput.innerHTML = messageInput.value
-        messageAlarm.style.visibility = 'hidden'
-        messageOutput.style.visibility = 'visible'
-    }
-
-    else {
-        messageAlarm.innerHTML = 'Bitte gib eine Nachricht ein!';
-        messageOutput.style.visibility = 'hidden'
-        messageAlarm.style.visibility = 'visible'
+        default:
+            console.log('is FALSE');
+            messageAlarm.innerHTML = 'Bitte gib eine Nachricht ein!';
+            messageOutput.style.visibility = 'hidden'
+            messageAlarm.style.visibility = 'visible'
+            break;
     }
 }
 
