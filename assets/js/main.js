67d3e14e21d97f3c78eadcefe535aa695c88519e
psiegmund <patrick-siegmund@web.de>
2022-05-13 10:02:18 +0200

    added some ;

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,6 @@ const messageInput = document.getElementById('message-input');
 
 // function showtxt() {
 
-//     console.log(Boolean(messageInput.value));
 //     if (messageInput.value) {
 //         messageOutput.innerHTML = messageInput.value
 //         messageAlarm.style.visibility = 'hidden'
@@ -26,16 +25,16 @@ function showtxt() {
     switch (Boolean(messageInput.value)) {
         case true:
             console.log('is TRUE');
-            messageOutput.innerHTML = messageInput.value
-            messageAlarm.style.visibility = 'hidden'
-            messageOutput.style.visibility = 'visible'
+            messageOutput.innerHTML = messageInput.value;
+            messageAlarm.style.visibility = 'hidden';
+            messageOutput.style.visibility = 'visible';
             break;
 
         default:
             console.log('is FALSE');
             messageAlarm.innerHTML = 'Bitte gib eine Nachricht ein!';
-            messageOutput.style.visibility = 'hidden'
-            messageAlarm.style.visibility = 'visible'
+            messageOutput.style.visibility = 'hidden';
+            messageAlarm.style.visibility = 'visible';
             break;
     }
 }
